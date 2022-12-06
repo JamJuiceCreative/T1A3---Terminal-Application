efb9d7516f6fb8fa9e02e73573d7596dbf5c7503
JamJuiceCreative <jim@jamjuicecreative.com.au>
2022-12-06 14:26:48 +1000

    Figured out random card chooser

---
+++ single_player.py
@@ -0,0 +1,9 @@
+from os import system
+from list_of_challenges import challenge1, challenge2, challenge3
+
+single_player = ""
+
+def single_player():
+    print(challenge1.challenge)
+
+single_player()
--- throw_card.py
+++ throw_card.py
@@ -3,10 +3,8 @@ import random
 card_values = ["Ace", 1 ,2 ,3 ,5 ,8 , "King"]
 # create random number generator
 def throw_card():
-    print(random.choice(card_values))
+    print(random.sample(card_values, k=1))
     
 # print (card_values[5])
-
-print(card_values[6])
 
 throw_card()
