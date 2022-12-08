db5a293c0ef76922ff148ea4532ef01f79fd32b5
JamJuiceCreative <jim@jamjuicecreative.com.au>
2022-12-08 11:13:04 +1000

    determining how many players in game

--- players.py
+++ players.py
@@ -1,7 +1,17 @@
 from player_class import Player
 
-player1 = Player("Survivor 1")
-player2 = Player("Survivor 2")
-player3 = Player("Survivor 3")
+# def Player_data():
+#     player1 = Player("Survivor 1")
+#     player2 = Player("Survivor 2")
+#     player3 = Player("Survivor 3")
 
-print(player1.name)
+
+   
+# player_list = [Player_data()]
+
+# print(player_list)
+
+def creating_list():
+    return [x for x in range (6)]
+counts = creating_list()
+print(f"The returned list is: ', {counts}")
---
+++ single_player.py
@@ -0,0 +1,17 @@
+from players import Player, Player_data
+# here is where the single player mode will run
+
+
+
+# def single_player_mode():
+#     print ("here we go!")
+#     for index in range(len(Playerlist)):
+#         if len(Playerlist) == 3:
+#             return (len(Playerlist))
+#         else:
+#             return ("buttholes")
+
+
+# single_player_mode()
+
+print (len(player_list))
