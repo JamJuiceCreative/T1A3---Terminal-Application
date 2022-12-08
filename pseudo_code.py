a315ea3a141cf36c6357f9d2456b17df04f2d787
JamJuiceCreative <jim@jamjuicecreative.com.au>
2022-12-08 10:25:47 +1000

    single player pseudo_code

---
+++ pseudo_code.py
@@ -0,0 +1,23 @@
+# Game intro 
+ # done
+
+# Single Player mode
+  # Show that it is Player 1's turn
+  # Throw Card prompt
+  # Once player has thrown card, store value and next players turn
+  # check Values
+  # if values don't all match, back to player 1's turn
+  # declare that only values equal to, less than highest card thrown or greater than lowest card thrown may be thrown.
+  # check values
+  # if values don't match repeat
+  # if value match, check card score against challenge score
+  # if card score equal to or greater than challenge score add challenge score and move on to next challenge
+  # Once all challenges are finished game is over, check player score and add to high score
+  # exit to main menu 
+ 
+
+# Multiplayer mode
+
+# rules & instructions
+
+# Exit the game
