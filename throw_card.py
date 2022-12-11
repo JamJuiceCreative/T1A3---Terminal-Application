172b6629c564b17aa21f9d714457e54a2744addb
JamJuiceCreative <jim@jamjuicecreative.com.au>
2022-12-11 23:12:45 +1000

    trying to set conditions for throw random card

--- throw_card.py
+++ throw_card.py
@@ -1,11 +1,21 @@
 import random
+from player_turn import players_threw
+from players import number_of_players
+
+count = 1
 # assign values of cards able to be thrown
 def random_card():
-    card_values = ["ace", "2" ,"3" ,"5" ,"8" , "king"]
-    # create random number generator
-    def throw_card():
-        return(random.sample(card_values, k=1))
-        
+    global count
+    count +1
+    if count != len(number_of_players()):
+        card_values = ["ace", "2" ,"3" ,"5" ,"8" , "king"]
+        # create random number generator
+        def throw_card():
+            return(random.sample(card_values, k=1))
+    else:
+        card_values = ["Mad" ,"Hatter" ,"is" ,"coming"]
+        def throw_card():
+            return(random.sample(card_values, k=1))     
         
     return("".join(throw_card()))
 
