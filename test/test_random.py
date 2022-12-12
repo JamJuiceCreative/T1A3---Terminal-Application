2fa31cf5844302b52ede87eb72521d56eb46e6b9
JamJuiceCreative <jim@jamjuicecreative.com.au>
2022-12-12 10:11:03 +1000

    small breakthrough with bridging the sum_of_inputs data

--- test/test_random.py
+++ test/test_random.py
@@ -2,4 +2,13 @@ import random
 def test_random():
     print(random.randint(0,9))
 
+    def test_add_numbers(num1, num2):
+
+        return (num1 + num2)
+
+    assert test_add_numbers(2,2) == 4
+
 test_random()
+
+
+
